7052400b3beaefd38491d1efd277d4821908db72
phister <nguyen1.618@gmail.com>
2019-02-25 23:01:35 +0100

    Finalize package functions

--- R/thesis.r
+++ R/thesis.r
@@ -1,8 +1,7 @@
 #' Creates an R Markdown PDF Thesis document
 #'
 #' This is a function called in output in the YAML of the driver Rmd file to
-# 'specify using the HU Berlin School of Business and Economics LaTeX
-#' template.
+#' specify using the HU Berlin School of Business and Economics LaTeX template.
 #'
 #' @export
 #' @param toc A Boolean (TRUE or FALSE) specifying whether table of contents
